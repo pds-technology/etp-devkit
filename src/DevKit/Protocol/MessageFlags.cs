4acf270885917bdc825de46462567b859dd076a1
Bobby Diaz <bobby.diaz@pds.nl>
2016-10-03 14:06:49 -0500

    Updated value of MessageFlags.FinalPart to equal 0x3 since it should be composed of 0x1 and 0x2.
    Related Work Items: #7366

--- src/DevKit/Protocol/MessageFlags.cs
+++ src/DevKit/Protocol/MessageFlags.cs
@@ -37,9 +37,9 @@ namespace Energistics.Protocol
         MultiPart = 0x1,
 
         /// <summary>
-        /// The final part of a multi-part message.
+        /// The final part of a multi-part message, componsed of: 0x1 | 0x2.
         /// </summary>
-        FinalPart = 0x2,
+        FinalPart = 0x3,
 
         /// <summary>
         /// No data is available.
